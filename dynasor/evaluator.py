e8efdcfd589d6716687cfe99e320c8cd399ec165
yichao <1793152758@qq.com>
2025-01-17 04:03:25 +0000

    framework

--- dynasor/evaluator.py
+++
@@ -1,8 +0,0 @@
-from dynasor.utils import load_jsonl
-
-class DataLoader:
-    def __init__(self, path: str):
-        self.data = load_jsonl(path)
-
-    def __len__(self):
-        return len(self.data)
